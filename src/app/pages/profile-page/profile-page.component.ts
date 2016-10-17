b154eeff2b19b89057c95c152a5f26e5745a618d
Jose A Dianes <jadianes@gmail.com>
2016-10-17 14:48:57 +0100

    listing user cloud credentials

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from 'ng2-cloud-portal-presentation-lib';
+import { ACCORDION_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'profile-page',
-  directives: [ ProfileComponent ],
+  directives: [ ProfileComponent, ACCORDION_DIRECTIVES, DROPDOWN_DIRECTIVES ],
   styles: [require('./profile-page.style.css')],
   template: require('./profile-page.template.html')
 })
