4027d6d645e3ce6f7157e42ef98c460181732b77
Jose A Dianes <jadianes@gmail.com>
2017-03-09 15:25:05 +0000

    deploy-destroy-remove sequence now working

--- src/app/pages/repository-page/repository-page.component.ts
+++ src/app/pages/repository-page/repository-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RepositoryComponent } from 'ng2-cloud-portal-presentation-lib';
-import { ACCORDION_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
-import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { ACCORDION_DIRECTIVES, DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { AlertComponent } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'repository-page',
-  directives: [ RepositoryComponent, ACCORDION_DIRECTIVES, DROPDOWN_DIRECTIVES ],
+  directives: [ RepositoryComponent, ACCORDION_DIRECTIVES, DROPDOWN_DIRECTIVES, AlertComponent ],
   styles: [require('./repository-page.style.css')],
   template: require('./repository-page.template.html')
 })
