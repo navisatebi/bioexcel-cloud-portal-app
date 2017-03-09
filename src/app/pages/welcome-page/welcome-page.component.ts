f7e7644238da25403d5336e6f72332b39f89ab56
Jose A Dianes <jadianes@gmail.com>
2017-03-09 16:34:50 +0000

    multiple cosmetic changes + context menu bar

--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { AboutPage } from '../about-page';
 import { LoginPage } from '../login-page';
-import { ProfilePage } from '../profile-page';
+import { RepositoryPage } from '../repository-page';
 import { CredentialService } from 'ng2-cloud-portal-service-lib';
 
 @Component({
   selector: 'welcome-page',
   template: require('./welcome-page.template.html'),
-  directives: [ AboutPage, LoginPage, ProfilePage ]
+  directives: [ AboutPage, LoginPage, RepositoryPage ]
 })
 export class WelcomePage {
   constructor( public credentialService: CredentialService ) {
