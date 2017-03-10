af44c77fbea0e6a851e5abd959f7fb57ef0bbb1c
Jose A Dianes <jadianes@gmail.com>
2017-03-10 14:22:45 +0000

    cosmetic updates + working profile related add forms

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from 'ng2-cloud-portal-presentation-lib';
-import { ACCORDION_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
-import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { DROPDOWN_DIRECTIVES, TAB_DIRECTIVES, ACCORDION_DIRECTIVES,
+  AlertComponent, MODAL_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'profile-page',
-  directives: [ ProfileComponent, ACCORDION_DIRECTIVES, DROPDOWN_DIRECTIVES ],
+  directives: [ ProfileComponent, AlertComponent,
+    ACCORDION_DIRECTIVES, DROPDOWN_DIRECTIVES, TAB_DIRECTIVES ],
   styles: [require('./profile-page.style.css')],
   template: require('./profile-page.template.html')
 })
