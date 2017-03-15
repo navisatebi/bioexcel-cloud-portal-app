477ac3391af65416326793c5025dc223308b172c
Jose A Dianes <jadianes@gmail.com>
2017-03-15 17:08:51 +0000

    UI fixes

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   template: require('./about-page.template.html')
 })
 export class AboutPage {
-
+  ebiLogoWhite = 'assets/img/EMBL_EBI_Logo_white.png';
+  ebiLogoBlack = 'assets/img/EMBL_EBI_Logo_black.png';
+  ebiLogoLarge = 'assets/img/embl-logo.png';
+  ebiLogoOnly = 'assets/img/embl_logo_only.png';
+  elixirLogo = 'assets/img/elixir_logo.png';
+  cloudsLogo = 'assets/img/clouds_logo.png';
 }
