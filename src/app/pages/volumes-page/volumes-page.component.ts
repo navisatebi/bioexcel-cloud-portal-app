b22d6e86ecad505c3d1b4ddb76475e57916d222b
Jose A Dianes <jadianes@gmail.com>
2017-03-17 11:29:43 +0000

    UI updates and fixes

--- src/app/pages/volumes-page/volumes-page.component.ts
+++ src/app/pages/volumes-page/volumes-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { VolumesComponent } from 'ng2-cloud-portal-presentation-lib';
 import { ACCORDION_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
-import { TAB_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { TAB_DIRECTIVES, AlertComponent } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'volumes-page',
-  directives: [ VolumesComponent, ACCORDION_DIRECTIVES, TAB_DIRECTIVES ],
+  directives: [ VolumesComponent, AlertComponent, ACCORDION_DIRECTIVES, TAB_DIRECTIVES ],
   styles: [require('./volumes-page.style.css')],
   template: require('./volumes-page.template.html')
 })
