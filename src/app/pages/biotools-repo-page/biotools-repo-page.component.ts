788a793fd6f73f421b66bdccf9a81d35ffe05c89
Jose A Dianes <jadianes@gmail.com>
2017-05-03 10:48:08 +0100

    a good bunch of UI improvements using Bootstrap specific classes

--- src/app/pages/biotools-repo-page/biotools-repo-page.component.ts
+++ src/app/pages/biotools-repo-page/biotools-repo-page.component.ts
@@ -82,6 +82,7 @@ export class BiotoolsRepoPage {
         {},
         {
           application_name: application.name,
+          disk_image: application.name,
           image_source_url: application.download[0].url,
           network_name: "test_network",
           floatingip_pool: "net_external"
