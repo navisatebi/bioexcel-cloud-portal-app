78a780af80c416792a2016052c271a6907a7e544
Jose A Dianes <jadianes@gmail.com>
2017-05-04 12:59:26 +0100

    changed network for BioExcel launcher

--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { AboutPage } from '../about-page';
 import { LoginPage } from '../login-page';
-import { RepositoryPage } from '../repository-page';
+import { BiotoolsRepoPage } from '../biotools-repo-page';
 import { CredentialService } from 'ng2-cloud-portal-service-lib';
 
 @Component({
   selector: 'welcome-page',
   template: require('./welcome-page.template.html'),
-  directives: [ AboutPage, LoginPage, RepositoryPage ]
+  directives: [ AboutPage, LoginPage, BiotoolsRepoPage ]
 })
 export class WelcomePage {
   constructor( public credentialService: CredentialService ) {
