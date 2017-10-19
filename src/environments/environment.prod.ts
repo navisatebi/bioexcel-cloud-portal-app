4f3a80ee15cb63199cf721eeab63ef1037fb7c38
Jose A Dianes <jadianes@gmail.com>
2017-10-19 11:28:03 +0100

    pointing to master API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiAddress: 'https://dev.api.portal.tsi.ebi.ac.uk/',
+  apiAddress: 'https://api.portal.tsi.ebi.ac.uk/',
   authAddress: 'https://api.aap.tsi.ebi.ac.uk/'
 };
