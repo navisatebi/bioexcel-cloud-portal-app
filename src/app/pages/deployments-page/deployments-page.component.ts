ea364854f6ad6fb78da35863c76c6d49d4e28b67
Jose A Dianes <jadianes@gmail.com>
2017-11-13 15:05:05 +0000

    added cloud resources view

--- src/app/pages/deployments-page/deployments-page.component.ts
+++ src/app/pages/deployments-page/deployments-page.component.ts
@@ -8,4 +8,52 @@ import { DeploymentsComponent } from 'ng2-cloud-portal-presentation-lib';
 })
 export class DeploymentsPageComponent {
 
+  fromTimeStampToDateAndTime(timestamp: number) {
+    let t = new Date( timestamp );
+    let formatted = t.toISOString().replace("T"," ");
+    let res = formatted.substring(0,formatted.indexOf("."));
+    return res;
+  }
+
+  getTimeToDisplay(millisec: number) {
+    let seconds: number = (millisec / 1000);
+    let secondsChar;
+    
+    let minutes: number = Math.floor(seconds / 60);
+    let minutesChar: string;
+
+    let hours: number = 0;
+    let hoursChar: string;
+
+    let days: number = 0;
+    let daysChar: string;
+
+    if (seconds > 59) {
+      if (minutes > 59) {
+          hours = Math.floor(minutes / 60);
+          if (hours>23) {
+            days = Math.floor(hours / 24);
+            daysChar = days.toFixed(0);
+            hours = hours - (days * 24);
+          }
+          hoursChar = (hours >= 10) ? hours.toFixed(0) : "0" + hours.toFixed(0);
+      }
+      minutes = minutes - (days * 24 * 60) - (hours * 60);
+      minutesChar = (minutes >= 10) ? minutes.toFixed(0) : "0" + minutes.toFixed(0);
+    }
+
+    seconds = Math.floor(seconds % 60);
+    secondsChar = (seconds >= 10) ? seconds.toFixed(0) : "0" + seconds.toFixed(0);
+
+    if (days > 0) {
+      return daysChar + " days " + hoursChar + "h " + minutesChar + "min " + secondsChar +"sec";
+    } else if (hours > 0) {
+      return hoursChar + "h " + minutesChar + "min " + secondsChar +"sec";
+    } else if (minutes > 0) {
+      return minutesChar + "min " + secondsChar +"sec";
+    } else {
+      return secondsChar +"sec";
+    }
+  }
+  
 }
