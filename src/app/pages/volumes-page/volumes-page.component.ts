98f23fabbaeacc86a01208dc9e8e7584766cd1ba
Felix Xavier Amaladoss <famaladoss@ebi.ac.uk>
2020-06-03 15:26:09 +0100

    Add Grafna dashboard

--- src/app/pages/volumes-page/volumes-page.component.ts
+++ src/app/pages/volumes-page/volumes-page.component.ts
@@ -10,14 +10,16 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class VolumesPageComponent {
   url1: string = "https://193.62.55.150:8888/sources/1/dashboards/1?refresh=Paused&lower=now%28%29%20-%2030d&present=true";
   url2: string = "https://193.62.55.150:8888/sources/1/dashboards/2?refresh=Paused&lower=now%28%29%20-%2030d&present=true";
+  grafanUrl: string = "https://193.62.55.150:3000/d/JAYnLgkGk/aap-user-statistics?orgId=1";
   urlSafe1: SafeResourceUrl;
   urlSafe2: SafeResourceUrl;
-
+  gurlSafe: SafeResourceUrl;
   constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.urlSafe1= this.sanitizer.bypassSecurityTrustResourceUrl(this.url1);
     this.urlSafe2= this.sanitizer.bypassSecurityTrustResourceUrl(this.url2);
+    this.gurlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.grafanUrl);
   }
 
 }
